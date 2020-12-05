d1f26aa1bb420cc29f34208b9a41d7126efd3adc
Edelveysa <51116291+Edelveysa@users.noreply.github.com>
2020-12-05 17:57:17 +0300

    Update Bublick.java

--- src/kids/Bublick.java
+++ src/kids/Bublick.java
@@ -1,28 +1,27 @@
 package kids;
 
 import enums.Location;
+import events.EventLibrary;
 
 import java.util.Objects;
 
 public final class Bublick extends Citizen{
-    private final String remembrance = " побывал в " + Location.ZMEEVKA.getLocation();
+    private String remembrance = " побывал в " + Location.ZMEEVKA.getLocation();
 
     public Bublick() {
         super("Бублик", Location.GREENTOWN);
     }
 
-    public void driveWith(Kid[] passengers){
+    public void driveWith(Kid[] passengers, EventLibrary eventLibrary){
         Mechanic vintik = (Mechanic) passengers[0];
         Mechanic shpuntik = (Mechanic) passengers[1];
-        System.out.println(getName() + " решил поехать c " + shpuntik.getName() + " и "+ vintik.getName() +
-                " в " + Location.ZMEEVKA.getLocation());
         vintik.setLocation(Location.ZMEEVKA);
         shpuntik.setLocation(Location.ZMEEVKA);
         setLocation(Location.ZMEEVKA);
         vintik.find();
         shpuntik.find();
-        vintik.setRemembrance(vintik.getName() + getRemembrance());
-        shpuntik.setRemembrance(shpuntik.getName() + getRemembrance());
+        eventLibrary.addEvent(vintik.getName() + getRemembrance());
+        eventLibrary.addEvent(shpuntik.getName() + getRemembrance());
     }
 
     public String getRemembrance() {
