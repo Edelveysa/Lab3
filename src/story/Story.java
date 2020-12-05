ed9385a893454d1fb908502a3f54890f17b6376d
Edelveysa <51116291+Edelveysa@users.noreply.github.com>
2020-12-05 18:01:09 +0300

    Update Story.java

--- src/story/Story.java
+++ src/story/Story.java
@@ -1,7 +1,12 @@
 package story;
 
 import enums.Location;
+import events.EventLibrary;
+
 import kids.*;
+
+import java.util.Arrays;
+
 
 public class Story {
 
@@ -10,25 +15,22 @@ public class Story {
         Mechanic shpuntik = new Mechanic("Шпунтик", Location.FLOWERTOWN);
         Citizen citizen = new Citizen();
         Znayka znayka = new Znayka();
+        Bublick bublick = new Bublick();
+        Kid[] witnesses = {vintik, shpuntik, citizen, bublick, znayka};
+        EventLibrary eventLibrary = new EventLibrary(witnesses);
         Balloon balloon = new Balloon();
-        citizen.addInMemory(balloon.getRemembrance());
+        eventLibrary.addEvent(balloon.getRemembrance());
         vintik.setLocation(Location.GREENTOWN);
         shpuntik.setLocation(Location.GREENTOWN);
-        Bublick bublick = new Bublick();
         Kid[] passengers = {vintik, shpuntik};
-        bublick.driveWith(passengers);
-        citizen.addInMemory(vintik.getRemembrance());
-        citizen.addInMemory(shpuntik.getRemembrance());
-        String[] memoryForFind = {balloon.getRemembrance(), vintik.getRemembrance(), shpuntik.getRemembrance()};
-        znayka.find(citizen, memoryForFind);
+        bublick.driveWith(passengers, eventLibrary);
+        znayka.find(citizen, eventLibrary);
 
     }
 
-    static public int random(){
+    public static int random(){
         return (int)(Math.round(Math.random()*100));
     }
-
-
 
 
 }
